fd31ad77b8823986708aafd187dade4e5eab3aad
srujan <srujan18@hotmail.com>
2020-04-21 15:11:04 -0400

    second commit

--- src/main/scala/ca/mcit/bigdata/course2/project/data/DataWriter.scala
+++ src/main/scala/ca/mcit/bigdata/course2/project/data/DataWriter.scala
@@ -1,17 +1,24 @@
 package ca.mcit.bigdata.course2.project.data
 
-import java.io.{File, FileWriter}
+import java.io.{File, FileWriter, OutputStreamWriter}
 
 import ca.mcit.bigdata.course2.project.proccesed.EnrichTrip
 import com.opencsv._
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 
 class DataWriter(enrichedList: List[EnrichTrip]) {
   println("filtered data")//+element.tripRoute.trips.service_id)
+  val conf = new Configuration()
+  val fs:FileSystem= FileSystem.get(conf)
+  val outputPath = new Path ("/user/fall2019/srujan/stm/SrujanfinalOutput.csv")
+  val out = new OutputStreamWriter( fs.create(outputPath))
+          if (fs.exists(outputPath))
+         fs.delete(outputPath, true)
 
-  val outputPath = "/home/bd-user/project_result_csv/SrujanfinalOutput.csv"
-  var file: File = new File(outputPath)
-  val output: FileWriter = new FileWriter(file)
-  val writer: CSVWriter = new CSVWriter(output)
+  //var file: File = new File(outputPath)
+ // val outsput: FileWriter = new FileWriter(file)
+  val writer: CSVWriter = new CSVWriter(out)
   val outputfile: Array[String] = Array("Route Id", "Service Id", "Trip Id", "Trip Head Sign", "Direction Id",
     "Shape Id", "Wheelchair accessible", "Note_FR", "Note En", "Agency Id",
     "Route Short Name", "Route Long Name", "Route Type", "Route Url", "Route Colour",
@@ -23,7 +30,7 @@ class DataWriter(enrichedList: List[EnrichTrip]) {
 
     writer.writeNext(outputfile)
     enrichedList.foreach { element => {
-      val data = Array(element.tripRoute.routes.route_id.toString, element.calender.service_id.toString,
+      val data = Array(element.tripRoute.routes.route_id.toString, element.calender.service_id,
         element.tripRoute.trips.trip_id.toString, element.tripRoute.trips.trip_headsign.toString,
         element.tripRoute.trips.direction_id.toString, element.tripRoute.trips.shape_id.toString,
         element.tripRoute.trips.wheelchair_accessible.toString, element.tripRoute.trips.note_fr.getOrElse().toString,
--- src/main/scala/ca/mcit/bigdata/course2/project/data/GetData.scala
+++ src/main/scala/ca/mcit/bigdata/course2/project/data/GetData.scala
@@ -1,8 +1,5 @@
 package ca.mcit.bigdata.course2.project.data
-
 import java.io.BufferedInputStream
-
-
 import ca.mcit.bigdata.course2.project.inputdata.{Calender, Route, Trip}
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
@@ -17,24 +14,21 @@ class GetData {
   val hadoop:FileSystem = FileSystem.get(conf)
 
 
-  val filePathRoutes = new BufferedInputStream( hadoop.open("/user/fall2019/srujan/routes.txt"))
+  val filePathRoutes = new BufferedInputStream( hadoop.open (new Path("/user/fall2019/srujan/stm/routes.txt")))
   //noinspection SourceNotClosed
-  def gRouteList: List[Route] = Source.fromFile(filePathRoutes).getLines().drop(1)
+  def gRouteList: List[Route] = Source.fromInputStream(filePathRoutes).getLines().drop(1)
     .map(line => line.split(","))
     .map(a => Route(a(0).toInt, a(1), a(2), a(3), a(4).toInt, a(5), a(6))).toList
 
-  val filePathCalender = new BufferedInputStream( hadoop.open("/user/fall2019/srujan/calender.txt"))
+  val filePathCalender = new BufferedInputStream( hadoop.open(new Path("/user/fall2019/srujan/stm/calendar.txt")))
   //noinspection SourceNotClosed
   def getCalenderList: List[Calender] = {
-    Source.fromFile(filePathCalender)
-      .getLines()
-      .drop(1)
-      .map(line => line.split(","))
+    Source.fromInputStream(filePathCalender).getLines().drop(1).map(line => line.split(","))
       .map(a => Calender(a(0), a(1), a(2), a(3), a(4), a(5), a(6), a(7), a(8), a(9))).toList
   }
-  val filePathTrips = new BufferedInputStream( hadoop.open("/user/fall2019/srujan/trips.txt"))
+  val filePathTrips = new BufferedInputStream( hadoop.open(new Path("/user/fall2019/srujan/stm/trips.txt")))
   //noinspection SourceNotClosed
-  def getTripList: List[Trip] = Source.fromFile(name = filePathTrips)
+  def getTripList: List[Trip] = Source.fromInputStream(filePathTrips)
     .getLines()
     .drop(1)
     .map(line => line.split(","))
